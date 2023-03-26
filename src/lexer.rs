b58b3e0eec3c1f3a2c649a162e455e144c1f8792
decqyd <68643853+decqyd@users.noreply.github.com>
2023-03-26 19:57:58 +0100

    cargo clippied

--- src/lexer.rs
+++ src/lexer.rs
@@ -10,34 +10,37 @@ use std::fmt::format;
 use std::ptr::null;
 pub struct Lexer;
 
+impl Default for Lexer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Lexer {
     pub fn new() -> Self {
         Self
     }
 
-    pub fn lex<'a>(self: &'a Self, s: &'a str, linenumber: i32) -> Vec<HashMap<Token, &str>> {
+    pub fn lex<'a>(&'a self, s: &'a str, linenumber: i32) -> Vec<HashMap<Token, &str>> {
         let mut splits = vec![];
 
         if Token::new(s) == Token::NEWLINE {
             splits.push(HashMap::from([(Token::NEWLINE, "\n")]));
-        } else if Token::new(s.chars().nth(0).unwrap().to_string().as_str()) == Token::STRING {
+        } else if Token::new(s.chars().next().unwrap().to_string().as_str()) == Token::STRING {
             let c = self.consume_until(&s[1..], '"', false, linenumber);
             splits.push(HashMap::from([(Token::STRING, c)]))
-        } else if s.chars().nth(0).unwrap().to_string().as_str() == "f"
+        } else if s.chars().next().unwrap().to_string().as_str() == "f"
             && Token::new(s.chars().nth(1).unwrap().to_string().as_str()) == Token::STRING
         {
             // TODO: add support for multiple variables
-            let index = match Self::find_char(&self, s, '{') {
-                Some(e) => e,
-                None => -1,
-            };
+            let index = Self::find_char(self, s, '{').unwrap_or(-1);
 
             let fvalue = if index != -1 {
                 self.consume_until(&s[index as usize + 1..], '}', false, linenumber)
             } else {
                 Error::new(
                     ErrorType::TypeError,
-                    format!("Unneeded f string"),
+                    "Unneeded f string".to_string(),
                     linenumber,
                 );
                 unreachable!("shouldnt")
@@ -50,28 +53,30 @@ impl Lexer {
                 (Token::OTHER, stc),
             ]))
         } else {
-            let split: Vec<&str> = s.split(" ").collect();
+            let split: Vec<&str> = s.split(' ').collect();
             let f = split[0];
 
             // if token is let
             if Token::new(f) == Token::IDENTIFIER {
                 let varname = split[1];
                 if Token::new(split[2]) == Token::ASSIGNMENT {
-                    let varvalue = if split[3] != "" {
+                    let varvalue = if !split[3].is_empty() {
                         split[3]
                     } else {
-                        Error::new(ErrorType::TokenError, format!("Missing value"), linenumber);
+                        Error::new(
+                            ErrorType::TokenError,
+                            "Missing value".to_string(),
+                            linenumber,
+                        );
                         unreachable!("program should end when error produces")
                     };
                     let t: &str;
-                    if varvalue.chars().nth(0).unwrap() == '"' {
+                    if varvalue.starts_with('"') {
                         t = "string";
+                    } else if varvalue.contains('.') {
+                        t = "float";
                     } else {
-                        if varvalue.contains(".") {
-                            t = "float";
-                        } else {
-                            t = "integer";
-                        }
+                        t = "integer";
                     }
 
                     let constructed = HashMap::from([
@@ -82,7 +87,11 @@ impl Lexer {
                     ]);
                     splits.push(constructed);
                 } else {
-                    Error::new(ErrorType::TokenError, format!("Missing \"=\""), linenumber);
+                    Error::new(
+                        ErrorType::TokenError,
+                        "Missing \"=\"".to_string(),
+                        linenumber,
+                    );
                     unreachable!("program should end when error produces")
                 };
             } else if f == "proc" {
@@ -94,7 +103,7 @@ impl Lexer {
     }
 
     pub fn consume_until<'a>(
-        self: &Self,
+        &self,
         s: &'a str,
         until: char,
         inclusive: bool,
@@ -117,7 +126,7 @@ impl Lexer {
                 &s[..=i]
             }
         } else {
-            let error = Error::new(
+            Error::new(
                 ErrorType::TokenError,
                 format!("Missing {until}"),
                 linenumber,
@@ -126,7 +135,7 @@ impl Lexer {
         }
     }
 
-    pub fn find_char(self: &Self, s: &str, find: char) -> Option<i32> {
+    pub fn find_char(&self, s: &str, find: char) -> Option<i32> {
         // TODO: make it so this fucntion can find multiple chars
 
         let mut i = 0;
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,10 @@
 #![allow(unused_imports, unused_variables, dead_code)]
 
+use crate::error::{Error, ErrorType};
 use crate::lexer::Lexer;
 use crate::tokens::Token;
 use std::collections::HashMap;
 use std::iter::Enumerate;
-
 pub struct Parser;
 
 impl Parser {
@@ -12,11 +12,11 @@ impl Parser {
         Self
     }
 
-    pub fn eval(self: Self, s: &str) {
+    pub fn eval(self, s: &str) {
         let mut vars: HashMap<&str, &str> = HashMap::new();
         let lexer = Lexer::new();
-        let mut linenum = 0;
-        for i in s.lines() {
+        for (linenum, i) in s.lines().enumerate() {
+            let linenum = linenum as i32;
             let res = lexer.lex(i, linenum);
             for h in &res {
                 for (i, k) in h.iter() {
@@ -27,21 +27,23 @@ impl Parser {
                     } else if i == &Token::FSTRING {
                         let fstring = h[&Token::FSTRING];
                         let stc = h[&Token::OTHER];
-
                         let fvalue = h[&Token::VARNAME];
 
                         let getvalue = match vars.get(&fvalue) {
                             Some(e) => e,
-                            None => "dooodoo",
+                            None => {
+                                Error::new(ErrorType::TokenError, "No token".to_string(), linenum);
+                                unreachable!("gurgle")
+                            }
                         };
-                        let formatted = fstring.replace(&stc, getvalue);
+
+                        let formatted = fstring.replace(stc, getvalue);
                         let stp =
                             Lexer::consume_until(&Lexer, &formatted[2..], '"', false, linenum);
                         println!(r#"{}"#, stp);
                     }
                 }
             }
-            linenum += 1;
         }
     }
 }
